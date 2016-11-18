17850f987c46b94ce1b22c3907afdaf04be9bd26
Chun-Yang <yang2007chun@gmail.com>
2016-11-18 09:57:58 -0600

    fix: typo

--- meteor-react-guard.js
+++ meteor-react-guard.js
@@ -10,7 +10,7 @@ export const GuardUser = createContainer(
   },
   ({ loadingElement, loginElement, status }) => {
     if (status === 'LOADING') {
-      return loadingComponent;
+      return loadingElement;
     }
     if (status === 'LOGGEDOUT') {
       return loginElement;
